33f3cda7210383e65ad5bce79ca2cde87a39b1cb
asmhossainbari <hossainbari@gmail.com>
2022-04-17 17:33:16 -0400

    Added folder for the solutions related to binary search
    - Added solution of the leetcode problem 704
    - Trivial binary search

---
+++ Binary_Search/704_Binary_Search.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            # print(nums[mid])
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                return mid
+        return -1
+
+sol = Solution()
+nums = [-1,0,3,5,9,12]
+target = 9
+print(sol.search(nums=nums, target=target))
