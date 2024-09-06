d38a6425893ac2a8c5e8ee52a4158d6353db8298
asmhossainbari <hossainbari@gmail.com>
2024-09-05 23:32:40 -0400

    O(NlogN) and O(NlogK) solutions of finding Kth largest element in array

--- Heap/215_kth_largest_element_in_array.py
+++ Heap/215_kth_largest_element_in_array.py
@@ -7,6 +7,8 @@ class Node(object):
        return self.val > other.val
 
 class Solution(object):
+    # time complexity: O(NlogN)
+    #
     def findKthLargest(self, nums, k):
         """
         :type nums: List[int]
@@ -20,6 +22,23 @@ class Solution(object):
         for i in range(k):
             k_th_largest_element = heapq.heappop(heap).val
         return k_th_largest_element
+
+    # time complexity: O(NlogK)
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        heap = []
+        for i in range(len(nums)):
+            if len(heap) < k:
+                heapq.heappush(heap, nums[i])
+            elif nums[i] > heap[0]:
+                heapq.heappop(heap)
+                heapq.heappush(heap, nums[i])
+        return heap[0]
+
 
 sol = Solution()
 print(sol.findKthLargest(nums=[3,2,1,5,6,4],k = 2))
