bb8c0bf45dd26330e2eaff9d0fe3646ea0d67f7a
asmhossainbari <hossainbari@gmail.com>
2024-09-10 19:40:38 -0400

    Revised the solution of leetcode 692 using min heap with O(NlogK) time complexity instead of O(NlogN) where K <= N

--- Heap/692_top_k_frequent_words.py
+++ Heap/692_top_k_frequent_words.py
@@ -1,4 +1,42 @@
+# import heapq
+# class WordFreq(object):
+#     def __init__(self, word, freq):
+#         self.word = word
+#         self.freq = freq
+#
+#     def __lt__(self, other):
+#         if self.freq == other.freq:
+#             return self.word < other.word
+#         else:
+#             return self.freq > other.freq
+#
+# class Solution(object):
+#     def topKFrequent(self, words, k):
+#         """
+#         :type words: List[str]
+#         :type k: int
+#         :rtype: List[str]
+#         """
+#         word_dict = dict()
+#         for word in words:
+#             if word not in word_dict:
+#                 word_dict[word] = 1
+#             else:
+#                 word_dict[word] += 1
+#         heap = []
+#         for key in word_dict.keys():
+#             heap_item = WordFreq(word=key, freq=word_dict[key])
+#             heapq.heappush(heap, heap_item)
+#         frequent_words = []
+#         for i in range(k):
+#             word = heapq.heappop(heap).word
+#             frequent_words.append(word)
+#         return frequent_words
+
 import heapq
+import collections
+import typing
+
 class WordFreq(object):
     def __init__(self, word, freq):
         self.word = word
@@ -8,32 +46,31 @@ class WordFreq(object):
         if self.freq == other.freq:
             return self.word < other.word
         else:
-            return self.freq > other.freq
-    
-class Solution(object):
+            return self.freq < other.freq
+
+
+class Solution:
     def topKFrequent(self, words, k):
-        """
-        :type words: List[str]
-        :type k: int
-        :rtype: List[str]
-        """
-        word_dict = dict()
+        hash_map = collections.defaultdict(int)
+        min_heap = []
         for word in words:
-            if word not in word_dict:
-                word_dict[word] = 1
-            else:
-                word_dict[word] += 1
-        heap = []
-        for key in word_dict.keys():
-            heap_item = WordFreq(word=key, freq=word_dict[key])
-            heapq.heappush(heap, heap_item)
-        frequent_words = []
+            hash_map[word] += 1
+        for word, freq in hash_map.items():
+            if len(min_heap) < k:
+                heapq.heappush(min_heap, WordFreq(word, freq))
+            elif min_heap[0].freq < freq:
+                heapq.heappop(min_heap)
+                heapq.heappush(min_heap, WordFreq(word, freq))
+
+        result = []
         for i in range(k):
-            word = heapq.heappop(heap).word
-            frequent_words.append(word)
-        return frequent_words
-    
+            result.append(heapq.heappop(min_heap).word)
+        result = result[::-1]
+        return result
+
+        # return [heapq.heappop(min_heap).word for _ in range(len(min_heap))][::-1]
+
 sol = Solution()
-print(sol.topKFrequent(words=["i", "love", "leetcode", "i", "love", "coding"], k=2))
+# print(sol.topKFrequent(words=["i", "love", "leetcode", "i", "love", "coding"], k=2))
 print(sol.topKFrequent(words=["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"], k = 4))
 print(sol.topKFrequent(words=["aaa","aa","a"], k = 1))
