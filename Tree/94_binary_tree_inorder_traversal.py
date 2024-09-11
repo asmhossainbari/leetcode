94faaeab2e315604f84527fc05239ba7272b9463
asmhossainbari <hossainbari@gmail.com>
2024-09-11 10:48:53 -0400

    Added solution of inorder traversal of binary tree using inner function which is called recursively

--- Tree/94_binary_tree_inorder_traversal.py
+++ Tree/94_binary_tree_inorder_traversal.py
@@ -1,9 +1,11 @@
+from typing import List, Optional
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution(object):
     def inorderTraversal(self, root):
         """
@@ -16,3 +18,15 @@ class Solution(object):
         val = root.val
         right_child = self.inorderTraversal(root.right)
         return left_child + [val] + right_child
+
+    def inorderTraversalRecursiveFunction(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        def inorder(root):
+            if root is None:
+                return root
+            inorder(root.left)
+            result.append(root.val)
+            inorder(root.right)
+
+        inorder(root)
+        return result
