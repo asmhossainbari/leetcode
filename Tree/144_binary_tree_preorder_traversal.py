4bf56975eafc28c47287688918d4929e7b38ac49
asmhossainbari <hossainbari@gmail.com>
2024-09-11 10:57:09 -0400

    Added solution of preorder traversal of binary tree using inner function which is called recursively

--- Tree/144_binary_tree_preorder_traversal.py
+++ Tree/144_binary_tree_preorder_traversal.py
@@ -1,9 +1,10 @@
+from typing import List, Optional
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution(object):
     def preorderTraversal(self, root):
         """
@@ -16,3 +17,15 @@ class Solution(object):
         left_child = self.preorderTraversal(root.left)
         right_child = self.preorderTraversal(root.right)
         return [val] + left_child + right_child
+
+    def preorderTraversalRecursiveFunction(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        def preorder(root):
+            if not root:
+                return root
+
+            result.append(root.val)
+            preorder(root.left)
+            preorder(root.right)
+        preorder(root)
+        return result
