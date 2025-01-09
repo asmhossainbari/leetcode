f434d4ea6fbb5a65961f4777e739973cfde61c88
asmhossainbari <hossainbari@gmail.com>
2025-01-09 18:55:21 -0500

    - Added BFS solution using queue and DFS solution using stack - Added time and space complexity for inverting binary tree for three approaches

--- Tree/226_Invert_Tree.py
+++ Tree/226_Invert_Tree.py
@@ -4,6 +4,52 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+'''
+BFS solution using queue
+Time complexity: O(n)
+Space complexity: O(n)
+'''
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        queue = deque([root])
+        while queue:
+            node = queue.popleft()
+            node.left, node.right = node.right, node.left
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+
+        return root
+
+'''
+DFS solution using stack
+Time complexity: O(n)
+Space complexity: O(n)
+'''
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            node.left, node.right = node.right, node.left
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        return root
+
+'''
+DFS solution using recursion
+Time complexity: O(n)
+Space complexity: O(n)
+'''
 class Solution(object):
     def invertTree(self, root):
         """
