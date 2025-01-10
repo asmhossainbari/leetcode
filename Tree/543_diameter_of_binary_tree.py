4b8c5930353379ea575f065ef06558c9c95d678e
asmhossainbari <hossainbari@gmail.com>
2025-01-10 16:51:11 -0500

    Solution to the Leetcode problem (543 Diameter of Binary Tree)

---
+++ Tree/543_diameter_of_binary_tree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+'''
+If n is the number of node in a binary tree,
+Time complexity: O(n)
+Space complexity: O(h), here h is height of the tree
+    If tree is balanced, h is log(n) in best case scenario
+    If tree is unbalanced, h could be n
+'''
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        # as diameter variable will be update in recursion, diameter variable is set to global variable by making a class member
+        self.diameter = 0
+
+        # we calculate the height of the node
+        # in addition to the height, we calculate diameter in between
+        def dfs(node):
+            if not node:
+                return 0
+
+            left_child_height = dfs(node.left)
+            right_child_height = dfs(node.right)
+            # Update the diameter if the path through the current node is longer
+            # except the following line, dfs function determines the height of the node
+            self.diameter = max(self.diameter, left_child_height + right_child_height)
+
+            return 1 + max(left_child_height, right_child_height)
+
+        dfs(root)
+        return self.diameter
