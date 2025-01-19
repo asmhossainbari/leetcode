91acf04fe0d84d7c6ea214eecfb285faffaba300
asmhossainbari <hossainbari@gmail.com>
2025-01-19 17:59:30 -0500

    Solution to the problem Leetcode 230(kth smallest element in BST) using recursive solution and iterative solutions

---
+++ Tree/230_kth_smallest_element_bst.py
@@ -0,0 +1,36 @@
+from typing import Optional
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        result = []
+        def dfs(root):
+            if not root:
+                return
+            dfs(root.left)
+            result.append(root.val)
+            dfs(root.right)
+        dfs(root)
+        # print(result)
+        return result[k - 1]
+
+
+class Solution2:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        stack = []
+        curr = root
+
+        while stack or curr:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            k -= 1
+            if k == 0:
+                return curr.val
+            curr = curr.right
