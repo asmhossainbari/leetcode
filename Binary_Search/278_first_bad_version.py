6515e2d0a0b6a53a2653202cc265ec0233ae5328
asmhossainbari <hossainbari@gmail.com>
2026-03-23 23:35:57 -0400

    revised the condition for better understanding

--- Binary_Search/278_first_bad_version.py
+++ Binary_Search/278_first_bad_version.py
@@ -8,10 +8,10 @@ class Solution:
         '''
         left = 1
         right = n
-        while left <= right:
+        while left < right:
             mid = left + (right - left) // 2
             if isBadVersion(mid):
-                right = mid - 1
+                right = mid
             else:
                 left = mid + 1
         return left
