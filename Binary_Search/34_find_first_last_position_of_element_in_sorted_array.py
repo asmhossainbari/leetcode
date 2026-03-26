cbf5ea409415e7826c8709de219ad581c7f9420c
asmhossainbari <hossainbari@gmail.com>
2026-03-25 20:41:03 -0400

    Refined binary search boundaries for LeetCode 34

--- Binary_Search/34_find_first_last_position_of_element_in_sorted_array.py
+++ Binary_Search/34_find_first_last_position_of_element_in_sorted_array.py
@@ -1,39 +1,41 @@
 from typing import List
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def binarySearchLeft(left_ptr, right_ptr):
+        def searchLeft(nums, target):
+            left = 0
+            right = len(nums) - 1
             first = -1
-            while left_ptr <= right_ptr:
-                mid_ptr = (left_ptr + right_ptr) // 2
-                if nums[mid_ptr] == target:
-                    first = mid_ptr
-                    right_ptr = mid_ptr - 1
-                elif target < nums[mid_ptr]:
-                    right_ptr = mid_ptr - 1
+            while left <= right:
+                mid = left + (right - left) // 2
+                if nums[mid] == target:
+                    right = mid - 1
+                    first = mid
+                elif nums[mid] > target:
+                    right = mid - 1
                 else:
-                    left_ptr = mid_ptr + 1
+                    left = mid + 1
             return first
 
-        def binarySearchRight(left_ptr, right_ptr):
+        def searchRight(nums, target):
+            left = 0
+            right = len(nums) - 1
             last = -1
-            while left_ptr <= right_ptr:
-                mid_ptr = (left_ptr + right_ptr) // 2
-                if nums[mid_ptr] == target:
-                    last = mid_ptr
-                    left_ptr = mid_ptr + 1
-                elif target < nums[mid_ptr]:
-                    right_ptr = mid_ptr - 1
+            while left <= right:
+                mid = left + (right - left) // 2
+                if nums[mid] == target:
+                    left = mid + 1
+                    last = mid
+                elif nums[mid] > target:
+                    right = mid - 1
                 else:
-                    left_ptr = mid_ptr + 1
+                    left = mid + 1
             return last
 
-        nums_len = len(nums)
-        left_most = binarySearchLeft(0, nums_len - 1)
-        right_most = binarySearchRight(0, nums_len - 1)
-
-
-        return [left_most, right_most]
-
+        leftmost = searchLeft(nums, target)
+        if leftmost == -1:
+            return [-1, -1]
+        rightmost = searchRight(nums, target)
+        return [leftmost, rightmost]
 
 sol = Solution()
 print(sol.searchRange(nums = [5,7,7,8,8,10], target = 8))
