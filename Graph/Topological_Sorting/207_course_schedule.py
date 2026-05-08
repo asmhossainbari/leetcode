b8239ef8faca39d511de20876db1248610c305be
asmhossainbari <hossainbari@gmail.com>
2026-05-08 12:55:22 -0400

    Fixed the bug in the graph representation and used deque instead of queue for faster execution

--- Graph/Topological_Sorting/207_course_schedule.py
+++ Graph/Topological_Sorting/207_course_schedule.py
@@ -32,6 +32,7 @@ class Solution:
                     return False
             visited.remove(crs)
             prerequisite_map[crs] = []
+            return True
 
         for i in range(numCourses):
             if not dfs(i):
@@ -43,43 +44,8 @@ BFS solution
 Kahn's algorithm
 Time complexity: O (V + E)
 Space complexity: O (V + E)
-where V is the number of courses and E is the number of prerequisites
-
 '''
 class Solution2:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        in_degree = [0] * numCourses
-        adjacency_list = [[] for i in range(numCourses)]
-        for src, dst in prerequisites:
-            in_degree[dst] += 1
-            adjacency_list[src].append(dst)
-
-        queue = deque()
-        for i in range(numCourses):
-            if in_degree[i] == 0:
-                queue.append(i)
-
-        finish_course = 0
-        while queue:
-            node = queue.popleft()
-            finish_course += 1
-            for neighbour in adjacency_list[node]:
-                in_degree[neighbour] -= 1
-                if in_degree[neighbour] == 0:
-                    queue.append(neighbour)
-
-        if finish_course == numCourses:
-            return True
-        else:
-            return False
-
-
-'''
-BFS solution
-Solution 3 implementation is much better than solution 2 though the time and space complexity is same.
-deque is not used here.
-'''
-class Solution3:
     def buildAdjacencyList(self, n, edgeList):
         adjacency_list = {i: [] for i in range(n)}
         for v1, v2 in edgeList:
@@ -89,16 +55,16 @@ class Solution3:
     def topoBFS(self, numCourses, prerequisites):
         adjacency_list = self.buildAdjacencyList(numCourses, prerequisites)
         in_degree = [0] * numCourses
-        for v1, v2 in prerequisites:
+        for v1, _ in prerequisites:
             in_degree[v1] += 1
-        queue = []
+        queue = deque()
         for v in range(numCourses):
             if in_degree[v] == 0:
                 queue.append(v)
 
         count = 0
         while queue:
-            node = queue.pop(0)
+            node = queue.popleft()
             count += 1
             for des in adjacency_list[node]:
                 in_degree[des] -= 1
@@ -110,7 +76,4 @@ class Solution3:
         return True
 
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        if self.topoBFS(numCourses, prerequisites):
-            return True
-        else:
-            return False
+        return self.topoBFS(numCourses, prerequisites)
